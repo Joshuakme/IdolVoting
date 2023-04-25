65772add9d0c499a7bd264e2544a0fb416764224
Joshuakme <joshuakoh23@gmail.com>
2023-04-25 15:57:11 +0800

    Update Votee CRUD functions

--- src/main/java/client/AdminMenu.java
+++ src/main/java/client/AdminMenu.java
@@ -154,54 +154,65 @@ public class AdminMenu {
                         
                         // If search result is not null
                         if(searchedVoteeList != null) {
-                            // Display the list of matchced votee
-                            System.out.println("\nBelow is the list of Votee that matched the entered name:");
-                            System.out.printf("%4s %-8s %-10s %-20s","No.", "Votee ID", "Votee Name", "Votee Desc");
-                            for(int i=0; i<searchedVoteeList.size(); i++) {
-                                System.out.printf("%4s %-8s %-10s %-20s", (i+1) + ". ", searchedVoteeList.get(i).getId(), searchedVoteeList.get(i).getName(), searchedVoteeList.get(i).getDescription());
-                            }
-
-                            // Get confirmed Votee
-                            System.out.print("Please select the Votee you would like to update: ");
-                            int selectedVoteeIndex = sc.nextInt();
-                            sc.nextLine();
-
-                            // Check if valid selectedVoteeIndex
-                            if(selectedVoteeIndex > 0 && selectedVoteeIndex < searchedVoteeList.size()) {
-                                // Get updated detail of the selected Votee
-                                System.out.println("Please enter the updated detail: ");
-
-                                System.out.println("Old: " + searchedVoteeList.get(selectedVoteeIndex).getId() + ", " + searchedVoteeList.get(selectedVoteeIndex).getName() + ", " +
-                                                    searchedVoteeList.get(selectedVoteeIndex).getDescription());
-                                System.out.print("New: " + searchedVoteeList.get(selectedVoteeIndex).getId() + ", ");
-
-                                String updatedVoteeDetails = sc.nextLine();
-
-                                // Split string by character ","
-                                String[] updatedVoteeDetailArr = updatedVoteeDetails.split(", ");
-
-                                // Check if the input is valid (exactly 2 parameter)
-                                if(updatedVoteeDetailArr.length == 2) {
-                                    Votee updatedVotee = new Votee(updatedVoteeDetailArr[0],updatedVoteeDetailArr[1]);
-                                    
-                                    // Update the selected Votee detail
-                                    admin.updateVotee(updatedVotee);
-                                    
-                                    // break loop
-                                    validUpdateVoteeInput = true;
-
-                                    // Successful Message
-                                    System.out.println("Updated Votee (" + updatedVotee.getName()+ ") updated successfully!!");
+                            boolean validselectedVoteeIndex = false;
+                            
+                            while(!validselectedVoteeIndex) {
+                                // Display the list of matchced votee
+                                System.out.println("\nBelow is the list of Votee that matched the entered name:");
+                                System.out.printf("%4s %-8s %-10s %-20s","No.", "Votee ID", "Votee Name", "Votee Desc");
+                                for(int i=0; i<searchedVoteeList.size(); i++) {
+                                    System.out.printf("%4s %-8s %-10s %-20s", (i+1) + ". ", searchedVoteeList.get(i).getId(), searchedVoteeList.get(i).getName(), searchedVoteeList.get(i).getDescription());
                                 }
-                                else if (updatedVoteeDetailArr.length < 2) {
-                                    System.err.println("\nYou have entered too few information. Please try again!\n");
+
+                                // Get confirmed Votee
+                                System.out.print("Please select the Votee you would like to update: ");
+                                int selectedVoteeIndex = sc.nextInt();
+                                sc.nextLine();
+
+                                // Check if valid selectedVoteeIndex
+                                if(selectedVoteeIndex > 0 && selectedVoteeIndex < searchedVoteeList.size()) {
+                                    boolean validUpdatedVoteeDetails = false;
+                                    
+                                    while(!validUpdatedVoteeDetails) {
+                                        // Get updated detail of the selected Votee
+                                        System.out.println("Please enter the updated detail: ");
+
+                                        System.out.println("Old: " + searchedVoteeList.get(selectedVoteeIndex).getId() + ", " + searchedVoteeList.get(selectedVoteeIndex).getName() + ", " +
+                                                            searchedVoteeList.get(selectedVoteeIndex).getDescription());
+                                        System.out.print("New: " + searchedVoteeList.get(selectedVoteeIndex).getId() + ", ");
+
+                                        String updatedVoteeDetails = sc.nextLine();
+
+                                        // Split string by character ","
+                                        String[] updatedVoteeDetailArr = updatedVoteeDetails.split(", ");
+
+                                        // Check if the input is valid (exactly 2 parameter)
+                                        if(updatedVoteeDetailArr.length == 2) {
+                                            Votee updatedVotee = new Votee(updatedVoteeDetailArr[0],updatedVoteeDetailArr[1]);
+
+                                            // Update the selected Votee detail
+                                            admin.updateVotee(updatedVotee);
+
+                                            // break loop
+                                            // Set all flags to true
+                                            validUpdatedVoteeDetails = true;        // This Level
+                                            validselectedVoteeIndex = true;         // Upper Level
+                                            validUpdateVoteeInput = true;           // Outmost Level
+
+                                            // Successful Message
+                                            System.out.println("Updated Votee (" + updatedVotee.getName()+ ") updated successfully!!");
+                                        }
+                                        else if (updatedVoteeDetailArr.length < 2) {
+                                            System.err.println("\nYou have entered too few information. Please try again!\n");
+                                        } else {
+                                            System.err.println("\nYou have entered too much information. Please try again!\n");
+                                        }  
+                                    } 
                                 } else {
-                                    System.err.println("\nYou have entered too much information. Please try again!\n");
-                                }  
-                            } else {
-                                // If invalid selectedVoteeIndex
-                                
-                                System.err.println("\nPlease enter a number that is in the range of (1 - " + searchedVoteeList.size() + "). Please try again\n");
+                                    // If invalid selectedVoteeIndex
+
+                                    System.err.println("\nPlease enter a number that is in the range of (1 - " + searchedVoteeList.size() + "). Please try again\n");
+                                }
                             }
                         } else {
                             // If search result is null
@@ -212,6 +223,71 @@ public class AdminMenu {
                     break;
                 case 3:
                     // Delete Votee
+                    boolean validDeleteVoteeInput = false;
+                    
+                    while(!validDeleteVoteeInput) {
+                        System.out.print("Please enter the Votee Name you would like to delete: ");
+                        String deleteVoteeName = sc.nextLine();
+                        
+                        // Get list of votee that matched the name of the entered name
+                        ListInterface<Votee> searchedVoteeList = admin.searchVotee(deleteVoteeName);
+                        
+                        
+                        // If search result is not null
+                        if(searchedVoteeList != null) {
+                            boolean validConfirmDeleteVoteeInput = false;
+                            
+                            while(!validConfirmDeleteVoteeInput) {    
+                                // Display the list of matchced votee
+                                System.out.println("\nBelow is the list of Votee that matched the entered name:");
+                                System.out.printf("%4s %-8s %-10s %-20s","No.", "Votee ID", "Votee Name", "Votee Desc");
+                                for(int i=0; i<searchedVoteeList.size(); i++) {
+                                    System.out.printf("%4s %-8s %-10s %-20s", (i+1) + ". ", searchedVoteeList.get(i).getId(), searchedVoteeList.get(i).getName(), searchedVoteeList.get(i).getDescription());
+                                }
+                                System.out.println("\n");
+
+                                // Get confirmed Votee
+                                System.out.print("Please select the Votee you would like to delete: ");
+                                int selectedVoteeIndex = sc.nextInt();
+                                sc.nextLine();
+
+                                // Check if valid selectedVoteeIndex
+                                if(selectedVoteeIndex > 0 && selectedVoteeIndex < searchedVoteeList.size()) {
+                                    // Print selected Votee detail
+                                    System.out.println("Below is the detail of the selected Votee: ");
+                                    System.out.println("Votee ID: " + searchedVoteeList.get(selectedVoteeIndex).getId() + "\n" +
+                                                        "Votee Name: " + searchedVoteeList.get(selectedVoteeIndex).getName() + "\n" +
+                                                        "Votee Description: " + searchedVoteeList.get(selectedVoteeIndex).getDescription() + "\n");
+
+                                    // Get confirmation from the admin
+                                    System.out.println("Do you confirm to delete the Votee? (Y/N/0) ");
+                                    char deleteVoteeConfirmation = Character.toUpperCase(sc.nextLine().charAt(0));
+
+                                    if(deleteVoteeConfirmation == 'Y') {
+                                        admin.deleteVotee(searchedVoteeList.get(selectedVoteeIndex).getId());
+                                        
+                                        // Set all flags to true
+                                        validConfirmDeleteVoteeInput = true;
+                                        validDeleteVoteeInput = true;
+                                    } else if(deleteVoteeConfirmation == 'N' || deleteVoteeConfirmation == 0) {
+                                        validConfirmDeleteVoteeInput = true;
+                                    } else {
+                                        System.err.println("Please enter a Y-Yes, N-No, 0-Return to last step");
+                                        validDeleteVoteeInput = true;
+                                    }
+
+                                } else {
+                                    // If invalid selectedVoteeIndex
+
+                                    System.err.println("\nPlease enter a number that is in the range of (1 - " + searchedVoteeList.size() + "). Please try again\n");
+                                }
+                            } 
+                        } else {
+                            // If search result is null
+                            
+                            System.err.println("\nThere is no Votee in the list that matched the entered name. Please try again\n");
+                        }
+                    }
                     break;
                 default:
                     break;
